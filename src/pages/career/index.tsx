f9b36d12da991a54c1d21e0ca98e6a7150a59532
A. Matías Quezada <amatiasq@gmail.com>
2022-07-06 02:00:56 +0200

    Add talks below career

--- src/pages/career/index.tsx
+++ src/pages/career/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from '../../components/atoms/Container.tsx';
-import { Heading3 } from '../../components/atoms/Heading.tsx';
+import { Heading2, Heading3 } from '../../components/atoms/Heading.tsx';
 import { Time } from '../../components/atoms/Time.tsx';
 import { TagList } from '../../components/molecules/TagList.tsx';
 import { AmqHeader } from '../../components/organisms/AmqHeader.tsx';
 import { AmqDocument } from '../../components/templates/AmqDocument.tsx';
 import { css } from '../../deps/emotion.ts';
 import { usePageUtils, Lang, RawHtml } from '../../generate/mod.ts';
+import { cssFontSize, cssSpace } from '../../theme.ts';
 import { getAllPagesBySection } from '../../util/getAllPagesBySection.ts';
 
 const { career, talks } = await getAllPagesBySection();
@@ -24,7 +25,7 @@ export default (props: any) => {
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
+    gap: ${cssSpace.lg};
   `;
 
   const timeStyles = css`
@@ -35,31 +36,70 @@ export default (props: any) => {
     font-size: 0.9rem;
   `;
 
+  const showDetailsStyle = css`
+    margin-top: 1rem;
+    cursor: pointer;
+  `;
+
+  const talkStyles = css`
+    ${headerStyles}
+    font-size: ${cssFontSize.md};
+    margin: 1rem 0;
+
+    &:last-child {
+      margin-bottom: 3rem;
+    }
+  `;
+
   return (
     <AmqDocument title="A. Matías Quezada" {...props}>
       <AmqHeader />
 
       <Container>
         <ol>
-          {career.map((item, index) => (
-            <li key={item.file} className={itemStyles}>
-              <Heading3 className={headerStyles}>
-                <Lang tr={item.title} />
-                <Time className={timeStyles} value={item.date} />
-              </Heading3>
+          {career.map((item, index) => {
+            const content = (
+              <>
+                {item.labels ? (
+                  <TagList className={tagsStyles} list={item.labels} />
+                ) : null}
 
-              {item.labels ? (
-                <TagList className={tagsStyles} list={item.labels} />
-              ) : null}
-
-              {index < SHOW_OPEN_ONLY_TOP ? (
                 <RawHtml html={item.content} />
-              ) : (
-                <details>
-                  <summary>Details</summary>
-                  <RawHtml html={item.content} />
-                </details>
-              )}
+              </>
+            );
+
+            return (
+              <li key={item.file} className={itemStyles}>
+                <Heading3 className={headerStyles}>
+                  <Time className={timeStyles} value={item.date} />
+                  <Lang tr={item.title} />
+                </Heading3>
+
+                {index < SHOW_OPEN_ONLY_TOP ? (
+                  content
+                ) : (
+                  <details>
+                    <summary className={showDetailsStyle}>Show details</summary>
+                    {content}
+                  </details>
+                )}
+              </li>
+            );
+          })}
+        </ol>
+
+        <Heading2>
+          <Lang en="Talks" es="Charlas" />
+        </Heading2>
+
+        <ol>
+          {talks.map((item) => (
+            <li key={item.file} className={talkStyles}>
+              <Time className={timeStyles} value={item.date} omitDay />
+              <Link page={item.file}>
+                <Lang tr={item.title} />
+              </Link>
+              {/* </Heading3> */}
             </li>
           ))}
         </ol>
